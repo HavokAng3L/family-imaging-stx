8f348c73d3ea1d069060d85f4579b9c5ee3d8236
HavokAng3L <angelmorin75@gmail.com>
2024-11-29 09:12:20 -0600

    Changing bundler due to issues with building for production

--- src/APIs/GenerateImages.js
+++
@@ -1,6 +0,0 @@
-export default getImages = async () => {
-  const response = await fetch("https://picsum.photos/2560/1440");
-  const data = response.url;
-
-  return data;
-};
--- src/pages/Services.jsx
+++
@@ -1,3 +0,0 @@
-export default Services = () => {
-  return <div>Services</div>;
-};
